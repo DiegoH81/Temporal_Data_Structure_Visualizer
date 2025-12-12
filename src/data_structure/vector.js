940075b5f05e9d601cdc6e95b23c1c9de6f26bd6
DiegoH81 <hidal8909ins@gmail.com>
2025-12-11 22:58:44 -0500

    feat(vector): Add vector

--- src/data_structure/vector.js
+++ src/data_structure/vector.js
@@ -1,1 +1,47 @@
+class Vector
+{
+    #data;
 
+    // Constructor
+    constructor()
+    {
+        this.#data = []
+    }
+    
+    // Functions
+    size()
+    {
+        return this.#data.length;
+    }
+
+    pushBack(inValue)
+    {
+        this.#data.push(inValue);
+    }
+
+    popBack()
+    {
+        return this.#data.pop();
+    }
+
+    insert(inIdx, inValue)
+    {
+        this.#data.splice(inIdx, 0, inValue);
+    }
+    
+    remove(inIdx)
+    {
+        this.#data.splice(inIdx, 1);
+    }
+
+    // Getter & setter
+    get(inIdx)
+    {
+        return this.#data[inIdx];
+    }
+
+    set(inIdx, inValue)
+    {
+        this.#data[inIdx] = inValue;
+    }
+}
